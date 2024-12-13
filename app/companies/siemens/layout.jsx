70815a0fcee0740b133357bda24d61f0c94c589c
Usama Iftikhar <usamaiftikhar718@gmail.comgit config user.email>
2024-12-13 18:08:46 +0500

    website updated

---
+++ app/companies/siemens/layout.jsx
@@ -0,0 +1,22 @@
+import FixedCallButtons from "@/components/buttons/FixCallButtons";
+import SiemensFixedContactButton from "@/components/sections/siemens/SiemensFixedContactButton";
+import SiemensFooter from "@/components/sections/siemens/SiemensFooter";
+import SiemensHeader from "@/components/sections/siemens/SiemensHeader";
+
+export const metadata = {
+  title: "Siemens Repair Center | Service Center UAE",
+  description: "Best home appliance repair service in Dubai and Abu Dhabi",
+};
+
+const SiemensLayout = ({ children }) => {
+  return (
+    <body className="flex flex-col text-black">
+      <SiemensHeader />
+      {children}
+      <SiemensFooter />
+      <FixedCallButtons />
+    </body>
+  );
+};
+
+export default SiemensLayout;
